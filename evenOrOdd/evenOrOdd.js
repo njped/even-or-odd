33ddbdee03cda718b049345e317776a5d4f3461a
Nathan Pedigo <njpedigo@gmail.com>
2023-04-26 11:59:34 -0600

    finished the evenOrOdd project

--- evenOrOdd/evenOrOdd.js
+++ evenOrOdd/evenOrOdd.js
@@ -1,37 +1,39 @@
 const total = 20
-let count = Number(process.argv[2])
+// let count = Number(process.argv[2])
 
 let num1 = 1;
-let num2 = 1;
+// let num2 = 1;
 
-let userInput = process.stdin
-userInput.setEncoding('utf-8')
+process.stdin.setEncoding('utf-8')
 console.log("Tell me even or odd")
-userInput.on('data', function (userInput) {
-  if (userInput === 'odd')
-  {
-    for(let i = 1; i <= total; i += 2) {
-      console.log(`You said: ${userInput}`)
-      console.log(num1 + `: counting: ` + i + '\n')
-      num1++
+process.stdin.on('readable', function () {
+  let userInput;
+  while((userInput = process.stdin.read()) !== null) {
+    if (userInput === 'odd\n')
+    {
+      for(let i = 1; i <= total; i += 2) {
+        console.log(`You said: ${userInput}`)
+        console.log(num1 + `: counting: ` + i)
+        num1++
+      }
     }
-  }
-  else if (userInput === 'even') {
-    for(let i = 1; i <= total; i += 2) {
-      console.log(`You said: ${userInput}`)
-      console.log(num1 + `: counting: ` + i + '\n')
-      num1++
+    else if (userInput === 'even\n') {
+      for(let i = 1; i <= total; i += 2) {
+        console.log(`You said: ${userInput}`)
+        console.log(num1 + `: counting: ` + i)
+        num1++
+      }
     }
-  }
-  else if (userInput === 'bye'){
-    console.log(`You said: ${userInput}`)
-    onsole.log('Bye. Thanks for sharing. [program signing out]')
-    process.exit()
-  }
-  else {
-    console.log(`You said: ${userInput}`)
-    console.log('please write: even or odd to continue')
-    console.log('type bye to exit the program')
+    else if (userInput === 'bye\n'){
+      console.log(`You said: ${userInput}`)
+      console.log('Bye. Thanks for sharing. [program signing out]')
+      process.exit()
+    }
+    else {
+      console.log(`You said: ${userInput}`)
+      console.log('please write: even or odd to continue')
+      console.log('type bye to exit the program')
+    }
   }
 })
 
